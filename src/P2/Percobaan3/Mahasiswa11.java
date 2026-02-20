2c06f7b1c61d4ada610f71aa889b92e8628f88f5
Valencza <143587759+Valencza@users.noreply.github.com>
2026-02-20 19:53:44 +0700

    P2_Percobaan3_11

---
+++ src/P2/Percobaan3/Mahasiswa11.java
@@ -0,0 +1,50 @@
+package P2.Percobaan3;
+
+public class Mahasiswa11 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa11() {
+
+        }
+
+    public Mahasiswa11(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIPK(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.5) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+}
