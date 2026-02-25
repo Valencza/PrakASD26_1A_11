172422ba62ff48bea94f880304db24478d560390
Valencza <143587759+Valencza@users.noreply.github.com>
2026-02-25 10:17:12 +0700

    P3_Percobaan3No3_11

---
+++ src/P3/Matakuliah/Matakuliah11.java
@@ -0,0 +1,15 @@
+package P3.Matakuliah;
+
+public class Matakuliah11 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah11(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
---
+++ src/P3/Matakuliah/MatakuliahDemo11.java
@@ -0,0 +1,31 @@
+package P3.Matakuliah;
+
+import java.util.Scanner;
+
+public class MatakuliahDemo11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Matakuliah11[] arrayOfMatakuliah = new Matakuliah11[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode : ");
+            kode = sc.nextLine();
+            System.out.print("Nama : ");
+            nama = sc.nextLine();
+            System.out.print("Sks: ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam: ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("------------------------------");
+
+            arrayOfMatakuliah[i] = new Matakuliah11(kode, nama, sks, jumlahJam);
+        }
+
+        sc.close();
+    }
+}
