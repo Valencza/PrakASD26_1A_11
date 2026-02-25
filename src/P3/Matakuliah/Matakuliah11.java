bf9f79f365d128d03f31c995eceb7c3f6d94f09b
Valencza <143587759+Valencza@users.noreply.github.com>
2026-02-25 19:38:55 +0700

    P3_Percobaan3_PertanyaanNo1_11

--- src/P3/Matakuliah/Matakuliah11.java
+++ src/P3/Matakuliah/Matakuliah11.java
@@ -6,21 +6,24 @@ public class Matakuliah11 {
     public int sks;
     public int jumlahJam;
 
-    public Matakuliah11() {
-        
-    }
-
     public Matakuliah11(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
-
-    void tambahData(String kode, String nama, int sks, int jumlahJam) {
+    
+    public Matakuliah11(String kode, String nama) {
         this.kode = kode;
         this.nama = nama;
-        this.sks = sks;
-        this.jumlahJam = jumlahJam;
+        this.sks = 0;
+        this.jumlahJam = 0;
+    }
+
+    public Matakuliah11() {
+        this.kode = "XXX";
+        this.nama = "Belum Ditentukan";
+        this.sks = 0;
+        this.jumlahJam = 0;
     }
 }
--- src/P3/Matakuliah/MatakuliahDemo11.java
+++ src/P3/Matakuliah/MatakuliahDemo11.java
@@ -9,24 +9,32 @@ public class MatakuliahDemo11 {
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Masukan Data Matakuliah ke-" + (i + 1));
-            System.out.print("Kode : ");
-            kode = sc.nextLine();
-            System.out.print("Nama : ");
-            nama = sc.nextLine();
-            System.out.print("Sks: ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam: ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
+        System.out.println("Input data matakuliah 1 (lengkap)");
+        System.out.print("Kode       : ");
+        kode = sc.nextLine();
+        System.out.print("Nama       : ");
+        nama = sc.nextLine();
+        System.out.print("SKS        : ");
+        sks = Integer.parseInt(sc.nextLine());
+        System.out.print("Jumlah Jam : ");
+        jumlahJam = Integer.parseInt(sc.nextLine());
+        arrayOfMatakuliah[0] = new Matakuliah11(kode, nama, sks, jumlahJam);
 
-            arrayOfMatakuliah[i] = new Matakuliah11();
-            arrayOfMatakuliah[i].tambahData(kode, nama, sks, jumlahJam);
+        System.out.println("------------------------------------");
 
-            System.out.println("---------------------------");
-        }
+        System.out.println("Input data matakuliah 2 (kode & nama saja)");
+        System.out.print("Kode : ");
+        kode = sc.nextLine();
+        System.out.print("Nama : ");
+        nama = sc.nextLine();
+        arrayOfMatakuliah[1] = new Matakuliah11(kode, nama);
+
+        System.out.println("------------------------------------");
+
+        System.out.println("Data matakuliah 3 menggunakan constructor default");
+        arrayOfMatakuliah[2] = new Matakuliah11();
+
+        System.out.println("====================================");
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
@@ -35,6 +43,11 @@ public class MatakuliahDemo11 {
             System.out.println("SKS :" + arrayOfMatakuliah[i].sks);
             System.out.println("Jumlah Jam :" + arrayOfMatakuliah[i].jumlahJam);
             System.out.println("-----------------------------");
+            System.out.println("Kode       : " + arrayOfMatakuliah[i].kode);
+            System.out.println("Nama       : " + arrayOfMatakuliah[i].nama);
+            System.out.println("SKS        : " + arrayOfMatakuliah[i].sks);
+            System.out.println("Jumlah Jam : " + arrayOfMatakuliah[i].jumlahJam);
+            System.out.println("------------------------------------");
         }
 
         sc.close();
