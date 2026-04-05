d84d771bcfbb69ec5a0bb89ce2497fb4e3a66487
Valencza <143587759+Valencza@users.noreply.github.com>
2026-04-05 18:44:07 +0700

    P6_Percobaan1_PoinC_11

--- src/P6/SortingMain11.java
+++ src/P6/SortingMain11.java
@@ -4,9 +4,11 @@ public class SortingMain11 {
     public static void main(String[] args) {
         int a[] = { 20, 10, 2, 7, 12 };
         int b[] = { 30, 20, 2, 8, 14 };
+        int c[] = { 40, 10, 4, 9, 3 };
 
         Sorting11 dataUrut1 = new Sorting11(a, a.length);
         Sorting11 dataUrut2 = new Sorting11(b, b.length);
+        Sorting11 dataUrut3 = new Sorting11(c, c.length);
 
         System.out.println("Data awal 1");
         dataUrut1.tampil();
@@ -17,8 +19,14 @@ public class SortingMain11 {
         System.out.println("Data awal 2");
         dataUrut2.tampil();
         dataUrut2.bubbleSort();
-        System.out.println("Data sudah diurutkan dengan Selection SORT (ASC)");
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataUrut2.tampil();
+
+        System.out.println("Data awal 3");
+        dataUrut3.tampil();
+        dataUrut3.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataUrut3.tampil();
 
     }
 }
