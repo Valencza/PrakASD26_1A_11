9c26ea4cd133dc33fdb076f765bcacc0ec24f9ee
Valencza <143587759+Valencza@users.noreply.github.com>
2026-04-05 16:53:24 +0700

    P5_Percobaan1_PertanyaanNo2_11

--- src/P5/Faktorial11.java
+++ src/P5/Faktorial11.java
@@ -1,23 +1,24 @@
 package P5;
 
 public class Faktorial11 {
-    
+
     int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
 
     int faktorialDC(int n) {
-        if (n==1) {
+        if (n == 1) {
             return 1;
-        }else {
-            int fakto = n * faktorialDC(n-1);
+        } else {
+            int fakto = n * faktorialDC(n - 1);
             return fakto;
         }
     }
 
-    
 }
