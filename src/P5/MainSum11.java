7561bb07ff287c44f7c78d1278d07f1332e464d6
Valencza <143587759+Valencza@users.noreply.github.com>
2026-04-05 17:49:25 +0700

    P5_Percobaan3_11

---
+++ src/P5/MainSum11.java
@@ -0,0 +1,24 @@
+package P5;
+
+import java.util.Scanner;
+
+public class MainSum11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan jumlah elemen: ");
+        int elemen = input.nextInt();
+
+        Sum11 sm = new Sum11(elemen);
+        for (int i = 0; i < elemen; i++) {
+            System.out.println("Masukkan keuntungan ke-" + (i + 1) + ": ");
+            sm.keuntungan[i] = input.nextDouble();
+        }
+
+        System.out.println("Total keuntungan menggunakan Bruteforce: " + sm.totalBF());
+        System.out.println(
+                "Total keuntungan menggunakan Devide and Conquer: " + sm.totalDC(sm.keuntungan, 0, elemen - 1));
+
+        input.close();
+    }
+}
