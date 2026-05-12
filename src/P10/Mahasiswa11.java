4569da7bb39c59dfd983abce8fa2be0427720097
Valencza <garciavalencza@gmail.com>
2026-05-12 20:55:11 +0700

    P10_Percobaan2_11

---
+++ src/P10/Mahasiswa11.java
@@ -0,0 +1,19 @@
+package P10;
+
+public class Mahasiswa11 {
+    String nim;
+    String nama;
+    String prodi;
+    String kelas;
+
+    public Mahasiswa11(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    void tampilkanData() {
+        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas);
+    }
+}
