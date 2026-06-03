c93dc3bfc94c5e2902e193cfae83e932504bc6ec
Valencza <garciavalencza@gmail.com>
2026-06-03 10:06:08 +0700

    P14_Percobaan1_11

---
+++ src/P14/Node11.java
@@ -0,0 +1,12 @@
+package P14;
+
+public class Node11 {
+    Mahasiswa11 mahasiswa;
+    Node11 left;
+    Node11 right;
+
+    public Node11(Mahasiswa11 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
