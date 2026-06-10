9e16e8e21f077b29e951ca0540a519d8d936cfb7
Valencza <garciavalencza@gmail.com>
2026-06-10 09:08:31 +0700

    P15_Percobaan1_PertanyaanNo2_11

--- src/P15/ContohList11.java
+++ src/P15/ContohList11.java
@@ -5,38 +5,39 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ContohList11 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        List l = new ArrayList();
-        l.add(1);
-        l.add(2);
-        l.add(3);
-        l.add("Cireng");
+                List<Integer> l = new ArrayList<>();
 
-        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
-                l.get(0), l.size(), l.get(l.size() - 1));
+                l.add(1);
+                l.add(2);
+                l.add(3);
+                l.add(4);
 
-        l.add(4);
-        l.remove(0);
+                System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
+                                l.get(0), l.size(), l.get(l.size() - 1));
 
-        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
-                l.get(0), l.size(), l.get(l.size() - 1));
+                l.add(5);
+                l.remove(0);
 
-        List<String> names = new LinkedList<>();
-        names.add("Noureen");
-        names.add("Akhleema");
-        names.add("Shannum");
-        names.add("Uwais");
-        names.add("Al-Qarni");
+                System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
+                                l.get(0), l.size(), l.get(l.size() - 1));
 
-        System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
-                names.get(0), names.size(), names.get(names.size() - 1));
+                List<String> names = new LinkedList<>();
+                names.add("Noureen");
+                names.add("Akhleema");
+                names.add("Shannum");
+                names.add("Uwais");
+                names.add("Al-Qarni");
 
-        names.set(0, "My kid");
+                System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
+                                names.get(0), names.size(), names.get(names.size() - 1));
 
-        System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
-                names.get(0), names.size(), names.get(names.size() - 1));
+                names.set(0, "My kid");
 
-        System.out.println("Names: " + names.toString());
-    }
+                System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
+                                names.get(0), names.size(), names.get(names.size() - 1));
+
+                System.out.println("Names: " + names.toString());
+        }
 }
